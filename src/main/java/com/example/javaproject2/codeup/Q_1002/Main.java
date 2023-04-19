04524de75f458807029ee2fd9f5a4961dafa0704
YunByungil <dbsquddlfz@naver.com>
2023-04-19 17:50:45 +0900

    feat: CodeUp 1001 ~ 1005 문제 완료

---
+++ src/main/java/com/example/javaproject2/codeup/Q_1002/Main.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup.Q_1002;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Q_1005/Main.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup.Q_1005;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }
+}
