976048b5110a2fb343f4d2819a7993e9d24737c3
YunByungil <dbsquddlfz@naver.com>
2023-04-20 09:59:40 +0900

    feat: CodeUp1006~1008 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Q_1007/Main.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup.Q_1007;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
