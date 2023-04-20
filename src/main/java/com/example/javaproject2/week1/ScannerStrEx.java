89966f7a80ec6d70932797dbb95a7a354e2995a1
YunByungil <dbsquddlfz@naver.com>
2023-04-20 09:40:52 +0900

    feat: ScannerExStr, ScannerExStrTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    public void readTwoStrAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
