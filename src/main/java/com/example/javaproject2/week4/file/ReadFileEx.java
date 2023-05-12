01ef284342281f0488a908c4d719ea76463572be
YunByungil <dbsquddlfz@naver.com>
2023-05-12 15:16:57 +0900

    feat: week4 - Day5 완료

--- src/main/java/com/example/javaproject2/week4/file/ReadFileEx.java
+++ src/main/java/com/example/javaproject2/week4/file/ReadFileEx.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 public class ReadFileEx {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\dbsqu\\Desktop\\건강보험심사평가원_전국 병의원 및 약국 현황 2022.9\\1.병원정보서비스 2022.10..csv"));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\dbsqu\\Desktop\\건강보험심사평가원_전국 병의원 및 약국 현황 2022.9\\1.병원정보서비스 2022.10..csv")));
+        System.out.println("br2 = " + br2.readLine());
         System.out.println("br.readLine() = " + br.readLine());
 
 
